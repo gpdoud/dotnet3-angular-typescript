aa2f43b4f505be1f4b16bf3f95a4db63cf347b7a
Greg Doud <gpdoud@gmail.com>
2018-09-20 06:34:09 -0400

    final commit

--- angular/my-cv/src/app/app.component.ts
+++ angular/my-cv/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from './services/user.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,29 @@ import { UserService } from './services/user.service';
 export class AppComponent implements OnInit {
   name: string = "Greg Doud";
   job: string = "Boot Camp Instructor";
-  email: string = "gdoud@maxtrain.com";
+  email: string = "gdoud@MAXtrain.com";
 
   constructor(private UserSvc: UserService) {}
 
-  ngOnInit() {
+  logToConsole() {
     this.UserSvc.List()
       .subscribe(users => {
         console.log(users);
-      });
+      });    
+  }
+
+  ngOnInit() {
+    let user: User 
+      = new User(1, 'MAX','MAX','MAX','MAX','MAX','MAX',true,true,true);
+    this.UserSvc.Change(user)
+      .subscribe(resp => {
+        console.log("Change response", resp);
+        this.logToConsole();
+      })
+
+    // this.UserSvc.Get("1")
+    //   .subscribe(user => {
+    //     console.log(user);
+    //   });
   }
 }
--- angular/my-cv/src/app/services/user.service.ts
+++ angular/my-cv/src/app/services/user.service.ts
@@ -12,6 +12,18 @@ export class UserService {
   List(): Observable<User[]> {
     return this.http.get(url+"List") as Observable<User[]>;
   }
+  Get(Id: string): Observable<User> {
+    return this.http.get(url+"Get/"+Id) as Observable<User>;
+  }
+  Create(user: User): Observable<any> {
+    return this.http.post(url+"Create", user) as Observable<any>;
+  }
+  Change(user: User): Observable<any> {
+    return this.http.post(url+"Change", user) as Observable<any>;
+  }
+  Remove(user: User): Observable<any> {
+    return this.http.post(url+"Remove", user) as Observable<any>;
+  }
 
   constructor(private http: HttpClient) { 
   }
